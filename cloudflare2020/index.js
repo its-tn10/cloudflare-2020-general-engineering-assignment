84373a1d79f7bea6b04778c061b147f2f931a516
its-tn10 <its.tn10@tamu.edu>
2020-10-10 04:02:42 +0000

    Final Modifications

--- cloudflare2020/index.js
+++ cloudflare2020/index.js
@@ -3,7 +3,7 @@
 /*******************************/
 
 // Array of Links for JSON API
-const URLs = [{'name': 'Website Home', 'url': 'https://tientavu.com/'}, {'name': 'LinkedIn', 'url': 'https://linkedin.com/in/tien-tavu'}, 
+const URLs = [{'name': 'Website', 'url': 'https://tientavu.com/'}, {'name': 'LinkedIn', 'url': 'https://linkedin.com/in/tien-tavu'}, 
               {'name': 'GitHub', 'url': 'https://github.com/its-tn10'}, {'name': 'YouTube', 'url': 'https://www.youtube.com/channel/UC8gmErEWus2TC3Tm_4U1yGQ'}];
 
 addEventListener('fetch', event => {
@@ -36,7 +36,7 @@ async function handleRequest(request) {
 
 async function handleHTMLRequest(request) {
   const response = await fetch('https://static-links-page.signalnerve.workers.dev');
-  return new HTMLRewriter().on("div", new LinksTransformer(URLs)).transform(response);
+  return new HTMLRewriter().on('*', new ElementTransformer(URLs)).transform(response);
 }
 
 /**
@@ -53,16 +53,29 @@ async function handleLinkRequest(request) {
 /*****    Class Objects    *****/
 /*******************************/
 
-class LinksTransformer {
+class ElementTransformer {
   constructor(links) {
     this.links = links;
   }
   
   async element(element) {
-    if (element.tagName == 'div' && element.getAttribute('id') == 'links') {
-      for (let i = 0; i < this.links.length; i++) 
-        element.append(this.links[i]['name'].link(this.links[i]['url']), {html: true});
-      
+    if (element.tagName == 'div') {
+      switch (element.getAttribute('id')) {
+        case 'links':
+          for (let i = 0; i < this.links.length; i++) 
+            element.append(this.links[i]['name'].link(this.links[i]['url']), {html: true});
+
+          break;
+
+        case 'profile':
+          element.removeAttribute('style');
+
+          break;
+      }
+    } else if (element.tagName == 'img' && element.getAttribute('id') == 'avatar') {
+      element.setAttribute('src', 'https://tientavu.com/static/media/bitmoji.png');
+    } else if (element.tagName == 'h1' && element.getAttribute('id') == 'name') {
+      element.setInnerContent('Tien Tavu');
     }
   }
 }
