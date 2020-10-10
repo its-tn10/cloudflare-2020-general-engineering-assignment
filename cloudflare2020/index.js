baaefbb565fb356d070774d9c4234030d1c29a2d
its-tn10 <its.tn10@tamu.edu>
2020-10-10 02:57:25 +0000

    JSON API Deployment

--- cloudflare2020/index.js
+++ cloudflare2020/index.js
@@ -1,12 +1,33 @@
+// Array of Links for JSON API
+const URLs = [{'name': 'Website Home', 'url': 'https://tientavu.com/'}, {'name': 'LinkedIn', 'url': 'https://linkedin.com/in/tien-tavu'}, 
+              {'name': 'GitHub', 'url': 'https://github.com/its-tn10'}, {'name': 'YouTube', 'url': 'https://www.youtube.com/channel/UC8gmErEWus2TC3Tm_4U1yGQ'}];
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const requestURL = new URL(request.url);
+  switch (requestURL.pathname) {
+    case '/links':
+      return handleLinkRequest(request);
+
+    default:
+      return new Response(null, {
+        headers: { 'content-type': 'text/plain' },
+      });
+  }
+}
+
+/**
+ * Handler for /links endpoint
+ */
+async function handleLinkRequest(request) {
+  return new Response(JSON.stringify(URLs), {
+    headers: { 'content-type': 'application/json' },
+  });
 }
